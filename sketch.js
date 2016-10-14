5d7b1cba4faa53ef5f3a957aa9b5f246feb2ecfd
marthematicist <hite.32@gmail.com>
2016-10-14 11:03:22 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,9 @@
 class Arm {
-  constructor() {
+  constructor( x , y ) {
+    this.center = createVector( x, y );
+    this.radius = random( minRadius , maxRadius );
+    this.angle = 0;
+    this.angVel = random( minAngVel , maxAngVel );
+    this.end = createVector( this.center.x + this.radius*sin( this.angle ) , this.center.x + this.radius*sin( this.angle ) );
   }
 }
