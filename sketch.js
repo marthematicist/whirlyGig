e029967ff5135567600161551837736b9bf339d4
marthematicist <hite.32@gmail.com>
2016-10-14 13:16:37 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,46 +24,46 @@ setupGlobalVariables = function() {
 }
 
 class Arm {
-  constructor( center ) {
-    this.center = createVector( center.x, center.y );
+  constructor( centerIn ) {
+    this.centerP = createVector( centerIn.x, centerIn.y );
     this.radius = random( minRadius , maxRadius );
     this.angle = 0;
     this.angVel = random( minAngVel , maxAngVel );
     if( random(0,1) > pClockwise ) {
       this.angVel *= -1;
     }
-    this.end = createVector( this.center.x + this.radius*cos( this.angle ) , this.center.y + this.radius*sin( this.angle ) );
+    this.end = createVector( this.centerP.x + this.radius*cos( this.angle ) , this.centerP.y + this.radius*sin( this.angle ) );
   }
 }
   
 Arm.prototype.rotate = function( dt ) {
   this.angle += this.angVel*dt;
-  this.end = createVector( this.center.x + this.radius*cos( this.angle ) , this.center.y + this.radius*sin( this.angle ) );
+  this.end = createVector( this.centerP.x + this.radius*cos( this.angle ) , this.centerP.y + this.radius*sin( this.angle ) );
 }
 
 
 class Chain {
-  contructor( center , nArms ) {
+  contructor( centerIn , nArms ) {
     this.n = nArms;
     console.log( 'Constructing new Chain' );
     this.color = color( random(0,100) , 100 , 100 , fillAlpha );
-    this.center = createVector( center.x , center.y );
-    var currentCenter = createVector( center.x , center.y );
+    this.center = createVector( centerIn.x , centerIn.y );
+    var currentCenter = createVector( centerP.x , centerP.y );
     for( var i = 0 ; i < nArms ; i++ ) {
       this.arms[i] = new Arm( currentCenter );
       currentCenter = this.arms[i].end;
     }
-    this.end = currentCenter;
+    this.endP = currentCenter;
     console.log(this.end.x);
   }
 }
   
 Chain.prototype.evolve = function( dt ) {
-  var currentCenter = this.center;
+  var currentCenter = this.centerP;
   for( var i = 0 ; i < this.n ; i++ ) {
-    this.arms[i].center = currentCenter;
+    this.arms[i].centerP = currentCenter;
     this.arms[i].rotate( dt );
-    currentCenter = this.arms[i].end;
+    currentCenter = this.arms[i].endP;
   }
   this.end = currentCenter;
 
@@ -72,8 +72,8 @@ Chain.prototype.evolve = function( dt ) {
 
 Chain.prototype.drawEnd = function() {
   //console.log( this.end );
-  var x = this.end.x;
-  var y = this.end.y;
+  var x = this.endP.x;
+  var y = this.endP.y;
   fill( this.color );
   ellipse( x , y , drawSize , drawSize );
 }
